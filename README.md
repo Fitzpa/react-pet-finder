0a6524001eff7b72ba998922f1aa1cd6794d87aa
Louie Fitzpatrick <fitz4492@gmail.com>
2020-06-03 15:09:40 -0500

    basic structure of react app

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # react-pet-finder
+This is a web app built with React, that utilizes the Petfinder AP
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { render } from "react-dom";
+
+const App = () => {
+
+  return (
+    <React.StrictMode>
+      <div>
+        
+      </div>
+    </React.StrictMode>
+  );
+};
+
+render(React.createElement(App), document.getElementById("root"));
